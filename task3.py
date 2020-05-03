feca4349d4ccfc41f6b9eefcd823ee2640339019
BedaSBa6koi <impalexxx@gmail.com>
2020-05-03 13:24:22 +0600

    Tasks

---
+++ task3.py
@@ -0,0 +1,6 @@
+a = int(input('Введи число: '))
+
+b = []
+for item in range (50):
+    if item % 3 == 0:
+        
---
+++ task4.py
@@ -0,0 +1,2 @@
+a = 'Given a string consisting of words separated by spaces'
+print(len(a.split(' ')))
